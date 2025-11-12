9bd2e86929586cde4b20e3c75f657dc58192772b
Edustartari <edustartari@gmail.com>
2025-11-11 23:28:57 -0300

    set redis

--- backend/views.py
+++ backend/views.py
@@ -4,6 +4,14 @@ import requests
 import json
 from django.conf import settings
 import os
+import redis
+
+redis_client = redis.Redis.from_url(settings.REDIS_URL)
+
+redis_client.set('test', json.dumps({
+	'message': 'This is a test message stored in Redis.'
+}), ex=30)
+test_info = redis_client.get('test')
 
 # Create your views here.
 def index(request):
--- mavi/settings.py
+++ mavi/settings.py
@@ -145,3 +145,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REDIS_URL = os.getenv('REDIS_URL')
